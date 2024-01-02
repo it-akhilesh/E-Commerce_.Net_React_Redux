e7ede2bdc07e8d90b39b975d1e68fe69eb71efbd
Akhilesh <360akhileshsingh.it@gmail.com>
2024-01-02 16:38:26 +0530

    Adding a delay and loading indicators to the app.

--- client/src/app/errors/NotFound.tsx
+++ client/src/app/errors/NotFound.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
         <Container component={Paper} sx={{height: 400}}>
             <Typography gutterBottom variant='h3'> Oops - we could noty find what you are looking for</Typography>
             <Divider />
-            <Button fullWidth component={Link} to='/catalog'>Go back to shop</Button>
+            <Button fullWidth component={Link}>Go back to shop</Button>
         </Container>
     )
 }
