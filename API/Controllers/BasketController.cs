f94134e593042440158ad4f7eb13786fd1043f0b
Akhilesh <360akhileshsingh.it@gmail.com>
2024-01-05 17:43:38 +0530

    Add basket item endpoint logic.

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -21,25 +21,31 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<Basket>> GetBasket()
         {
-            var basket = await _context.Baskets
-                .Include(i => i.Items)
-                .ThenInclude(p => p.Product)
-                .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+            var basket = await RetrieveBasket();
             if (basket == null) return NotFound();
 
-            return basket;    
+            return basket;
         }
 
+        
         [HttpPost]
 
         public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
         {
-            // get basket
-            // create basket
-            // get product
-            // add item 
-            // save changes
-            return StatusCode(201);
+            var basket = await RetrieveBasket();
+            
+            if (basket == null) basket = CreateBasket();
+
+            var product = await _context.Products.FindAsync(productId);
+
+            if (product == null) return NotFound();
+
+            basket.AddItem(product, quantity);
+
+            var result = await _context.SaveChangesAsync() > 0;
+            
+            if (result) return StatusCode(201);
+            return BadRequest(new ProblemDetails{Title = "Problem saving item to basket"});
         }
         [HttpDelete]
 
@@ -48,5 +54,23 @@ namespace API.Controllers
             // get basket
             return Ok();
         }
+
+        private async Task<Basket> RetrieveBasket()
+        {
+            return await _context.Baskets
+                            .Include(i => i.Items)
+                            .ThenInclude(p => p.Product)
+                            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+        }
+        private Basket CreateBasket()
+        {
+            var buyerId = Guid.NewGuid().ToString();
+            var cookieOptions = new CookieOptions{IsEssential = true, Expires = DateTime.Now.AddDays(30)};
+            Response.Cookies.Append("buyerId", buyerId, cookieOptions);
+            var basket = new Basket{BuyerId = buyerId};
+            _context.Baskets.Add(basket);
+            return basket;
+        }
+
     }
 }
